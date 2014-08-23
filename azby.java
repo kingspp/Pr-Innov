40fff8d956131126f1253a8bb19f580c29500b13
Prathyush <kingspprathyush@gmail.com>
2014-08-23 23:41:49 +0530

    Update azby.java

--- azby.java
+++ azby.java
@@ -1,3 +1,16 @@
+/*
+Created on Aug 21, 2014
+
+@author: Prathyush
+*/
+
+/*
+This program converts the input to uppercase and then prints the reverse of that character. 
+The program takes space into consideration.
+Example : i/p: MYSORE
+	  o/p: NBHLIV
+*/
+
 import java.util.Scanner;
 
 public class azby {
