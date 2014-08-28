76b61443ed6a5327bac5a06f6f169fb441e0d453
Prathyush <kingspprathyush@gmail.com>
2014-08-28 22:24:55 +0530

    Create Clock_Angle.py

---
+++ Clock_Angle.py
@@ -0,0 +1,27 @@
+'''
+Created on 28-Aug-2014
+
+@author: prathyush
+'''
+print "Clock Angle"
+dangle=0.0
+hr=int(raw_input("Enter the Hour: "))
+min=int(raw_input("Enter the Min: "))
+dir=int(raw_input("Enter the Direction \n 1.Clockwise \n 2.AntiClockwise \n:"))
+
+if dir==1:
+    dmin=min*6
+    dhr=(hr*30)+(0.5*min)
+    dangle=abs(dhr-dmin)
+    
+elif (dir==2):
+    if min==0:
+        min=60
+    dmin=60-(min)
+    t=dmin
+    dmin=dmin*6
+    dhr=12-hr
+    dhr=(dhr*30)+(0.5*t)
+    dangle=abs(dhr-dmin)
+    
+print dangle
