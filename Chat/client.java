dccb488a646bb8342008e453f5edf7514cb6023c
kingspp <kingspprathyush@gmail.com>
2014-09-04 22:18:34 +0530

    Added Client name

--- Chat/client.java
+++ Chat/client.java
@@ -1,25 +1,69 @@
-import java.net.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
 
 class Client
 {
+	private ObjectInputStream sInput;		// to read from the socket
+	private ObjectOutputStream sOutput;		// to write on the socket
+	private Socket s;
+	private static String server, username;
+	private static int port;
+	
+	
+
+	public boolean start() {
+		// try to connect to the server
+		try {
+			s = new Socket(server, port);
+		} 
+		// if it failed not much I can so
+		catch(Exception ec) {		
+			System.out.println("Error connectiong to server:" + ec);
+			System.exit(0);
+			return false;
+		}
+		String msg = "Connection accepted " + s.getInetAddress() + ":" + s.getPort();
+		System.out.println(msg);
+	
+		/* Creating both Data Stream */
+		try
+		{
+			sInput  = new ObjectInputStream(s.getInputStream());
+			sOutput = new ObjectOutputStream(s.getOutputStream());
+		}
+		catch (IOException eIO) {
+			System.out.println("Exception creating new Input/output Streams: " + eIO);
+			return false;
+		}
+
+		try
+		{
+			sOutput.writeObject(username);
+		}
+		catch (IOException eIO) {
+			System.out.println("Exception doing login : " + eIO);
+			
+			return false;
+		}
+		// success we inform the caller that it worked
+		return true;
+	}
+	
+	
+	
 	public static void main(String v[]) 
 	{
 		System.out.println("Client Started");
 		Scanner scan=new Scanner(System.in);
-		System.out.print("Enter the Port");;
-		int port=scan.nextInt();
-		//Socket s=new Socket("Hello",port);
-		
-		try{
-		Socket s=new Socket("localhost",port);
-		System.out.println(s);
-		}
-		catch (Exception ex){
-		System.out.println("Please Check the port");
-		System.exit(0);
-		}
-		
-		
+		System.out.print("Enter the Port");
+		int port=scan.nextInt();		
+		Client client=new Client();
+		client.port=1500;
+		client.server="localhost";
+		client.username="King";
+		client.start();
 	}
 }
--- Chat/server.java
+++ Chat/server.java
@@ -8,21 +8,34 @@ java server [port]
 
 */
 
-import java.net.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 class Server
 {
+	
+	private static String username; 
+	private static ObjectOutputStream sOutput;
+	private static ObjectInputStream sInput;
 	private static SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yy  HH:mm");
 	public static void main(String args[]) throws Exception
 	{
 		int port=1500;
+		Scanner scan=new Scanner(System.in);
 		String date= new Date().toString();
 		date = sdf.format(new Date()).toString();
 		
-		//if (args[0]!="\0") port=Integer.parseInt(args[0]);
-		//else port=1500;
+		if (args.length>0) 
+			port=Integer.parseInt(args[0]);
+		else 
+			System.out.println("Enter the port no: ");
+			port=scan.nextInt();
 		
 		System.out.println("Start of server..");
 		System.out.println(date);
@@ -31,12 +44,32 @@ class Server
 		
 		while(true)
 		{
+			
 			Socket s=null;
 			while(s==null)
 			{
 				s=obj.accept();
 				System.out.println(s);
-			}			
+			}		
+			
+			try
+			{
+				// create output first
+				sOutput = new ObjectOutputStream(s.getOutputStream());
+				sInput  = new ObjectInputStream(s.getInputStream());
+				// read the username
+				username = (String) sInput.readObject();
+				System.out.println(username + " just connected.");
+			}
+			catch (IOException e) {
+				System.out.println("Exception creating new Input/output Streams: " + e);
+				return;
+			}
+			// have to catch ClassNotFoundException
+			// but I read a String, I am sure it will work
+			catch (ClassNotFoundException e) {
+			}
+            date = new Date().toString() + "\n";
 		}
 		
 	}
