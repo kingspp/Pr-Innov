f7919c4301b7106efbc57511c351eb8faa55ed5b
kingspp <kingspprathyush@gmail.com>
2014-09-05 00:02:28 +0530

    Added ListenToServer

--- Chat/client.java
+++ Chat/client.java
@@ -1,28 +1,46 @@
+/* Created on 22-08-14
+ * @author Prathyush SP
+ * 
+ *  Commands:
+ *	java client [server address] [port] [username]
+ */
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-class Client
+class Client 
 {
 	private ObjectInputStream sInput;		// to read from the socket
 	private ObjectOutputStream sOutput;		// to write on the socket
 	private Socket s;
 	private static String server, username;
 	private static int port;
+	private Scanner scan=new Scanner(System.in);
 	
+	
+	public boolean initilize(){
+		System.out.print("Enter the Server Address: ");
+		server=scan.nextLine();
+		System.out.print("Enter the Port: ");
+		port=Integer.parseInt(scan.nextLine());
+		System.out.print("Enter the Username:  ");
+		username=scan.nextLine();	
+		System.out.println("");
+		return true;
+	}
 	
 
-	public boolean start() {
-		// try to connect to the server
+	public boolean start() {		
 		try {
 			s = new Socket(server, port);
-		} 
-		// if it failed not much I can so
+		}
+		
 		catch(Exception ec) {		
 			System.out.println("Error connectiong to server:" + ec);
-			System.exit(0);
+			initilize();
 			return false;
 		}
 		String msg = "Connection accepted " + s.getInetAddress() + ":" + s.getPort();
@@ -39,13 +57,13 @@ class Client
 			return false;
 		}
 
+		new ListenFromServer().start();
 		try
 		{
 			sOutput.writeObject(username);
 		}
 		catch (IOException eIO) {
-			System.out.println("Exception doing login : " + eIO);
-			
+			System.out.println("Exception doing login : " + eIO);			
 			return false;
 		}
 		// success we inform the caller that it worked
@@ -54,16 +72,53 @@ class Client
 	
 	
 	
-	public static void main(String v[]) 
+	public static void main(String args[]) 
 	{
 		System.out.println("Client Started");
-		Scanner scan=new Scanner(System.in);
-		System.out.print("Enter the Port");
-		int port=scan.nextInt();		
 		Client client=new Client();
-		client.port=1500;
-		client.server="localhost";
-		client.username="King";
+		if(args.length==3)
+		{
+			client.server=args[0];
+			client.port=Integer.parseInt(args[1]);
+			client.username=args[2];
+		}
+		
+		else		
+		client.initilize();
+		
 		client.start();
+		
+		
+		
+	}
+	
+	
+	class ListenFromServer extends Thread {
+
+		public void run() {
+			while(true) 
+				try {
+					String msg = (String) sInput.readObject();
+					// if console mode print the message and add back the prompt
+					
+						System.out.println(msg);
+						System.out.print("> ");
+					
+				}
+				catch(IOException e) {
+					System.out.println("Server has close the connection: " + e);
+					
+				}
+				// can't happen with a String object but need the catch anyhow
+				catch(ClassNotFoundException e2) {
+				}
+		}
 	}
 }
+
+
+
+
+
+
+
