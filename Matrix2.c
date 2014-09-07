e014387557ac7093758459b61e8cb5f1b5f9e04a
Prathyush <kingspprathyush@gmail.com>
2014-09-07 11:39:21 +0530

    Refactored the Program

--- Matrix2.c
+++ Matrix2.c
@@ -16,20 +16,22 @@ o/p: 1	2     3
 #include<stdio.h>
 #include<conio.h>
 
+//Global Declarations. To avoid array passing complexity
 int row,col;
 int i,j,k;
 int mat[100][100];
 int r=0,c=0;
 
+//Function to get input for the matrix
 void getIn()
 {
-printf("Enter the elements of the Matrix: \n");
-for(i=0;i<row;i++)
-for(j=0;j<row;j++)
-scanf("%d",&mat[i][j]);
+	printf("Enter the elements of the Matrix: \n");
+	for(i=0;i<row;i++)
+	for(j=0;j<row;j++)
+	scanf("%d",&mat[i][j]);
 }
 
-
+//Function to print the matrix
 void printMat()
 {
 printf("\nThe Final Matrix is: \n");
@@ -37,85 +39,65 @@ for(i=0;i<=row;i++)
 {
 	for(j=0;j<=col;j++)
 	{
-	if(j==col)
-	printf("\t");
+		if(j==col)
+			printf("\t");
 		printf("\t%d",mat[i][j]);
-
-	}
-	if(i==row-1)
-	printf("\n");
-	printf("\n");
-}
+		}
+		if(i==row-1)
+			printf("\n");
+		printf("\n");
+  	}
 printf("\n");
 }
 
+//To initialize the matrix with '32'
 void init()
 {
-for(i=0;i<100;i++)
-{
-	for(j=0;j<100;j++)
-	{
-		mat[i][j]=' ';
-	}
-}
+	for(i=0;i<100;i++)
+		for(j=0;j<100;j++)
+			mat[i][j]=' ';
 }
 
-
+//To compute the Rows and colum and the final sum
 void cal()
 {
-for(i=0;i<=row;i++)
-{
-	for(j=0;j<=col;j++){
-	if(j==col){
-	for(k=0;k<col;k++){
-	mat[i][j]+=mat[i][k];
-	//printf("%d\n",mat[i][j]);
-	}
-	mat[i][j]=mat[i][j]-32;
-	}
-
-	if(i==col){
-	for(k=0;k<col;k++){
-	mat[i][j]+=mat[k][j];
-	//printf("%d\n", mat[i][j]);
-	}
-	mat[i][j]=mat[i][j]-32;
-	}
-	}
+	for(i=0;i<=row;i++)
+		for(j=0;j<=col;j++)
+		{
+			if(j==col)
+			{
+				for(k=0;k<col;k++)
+					mat[i][j]+=mat[i][k];
+				mat[i][j]=mat[i][j]-32;
+			}
+		
+			if(i==col)
+			{
+				for(k=0;k<col;k++)
+					mat[i][j]+=mat[k][j];
+				mat[i][j]=mat[i][j]-32;
+			}
+		}
+	j=row;
+	for(i=0;i<row;i++)
+	r+=mat[i][j];
+	j=col;
+	for(i=0;i<row;i++)
+	c+=mat[j][i];
+	mat[j][j]=r+c;
 }
 
-j=row;
-for(i=0;i<row;i++)
-r+=mat[i][j];
-j=col;
-for(i=0;i<row;i++)
-c+=mat[j][i];
-mat[j][j]=r+c;
-}
-
-
+//Main Function
 void main()
 {
-
-
-clrscr();
-
-init();
-printf("Enter the no of Rows: ");
-scanf("%d",&row);
-printf("Enter the no of Columns: ");
-scanf("%d",&col);
-
-
-getIn();
-
-cal();
-printMat();
-
-
-
-
-
-getch();
-
+	clrscr();
+	init();
+	printf("Enter the no of Rows: ");
+	scanf("%d",&row);
+	printf("Enter the no of Columns: ");
+	scanf("%d",&col);
+	getIn();
+	cal();
+	printMat();
+	getch();
 }
