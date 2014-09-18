d740201a7e48ab5b77f466af9586f1db11fd5c0d
Prathyush <kingspprathyush@gmail.com>
2014-09-18 22:03:41 +0530

    Update JumbledArray.java

--- A-I-O/JumbledArray.java
+++ A-I-O/JumbledArray.java
@@ -1,3 +1,15 @@
+/*Created on 17-09-14
+@Author- Prathyush S.P
+
+This is an example of a jumbled array.
+Question: You have an array of 100 elements. The elements range from 1-100. The job is to find the missing number.
+1st approach: Ouer loop - 100 times. Inner loop - 100 times. Total loops 100*100.
+2nd approach: Sort the numbers from the beginning. Then iterate the loop to find the missing number. Total loops (Sorting)*100.
+**3rd approach: Get a boolean array. Equte the number with boolean index as true if found. Else false.**
+
+Note: Random is used to genetate 100 random numbers!!! LAZY :P
+
+*/
 import java.util.Random;
 
 public class JumbledArray {	
