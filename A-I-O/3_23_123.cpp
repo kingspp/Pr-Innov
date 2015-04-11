79d47eab0d94af7edb3d91bf7d7535fa4c7a4ec7
Prathyush <kingspprathyush@gmail.com>
2015-04-11 13:45:38 +0530

    Added Description

--- A-I-O/3_23_123.cpp
+++ A-I-O/3_23_123.cpp
@@ -1,5 +1,9 @@
-//Created on 28-Aug-2014
-//@author: prathyush
+/*Created on 28-Aug-2014
+@author: prathyush
+Description: This program prints the digts in recurring order.
+Example: Input : 5
+         Output: 5 45 345 2345 12345
+*/
 
 #include<iostream.h>
 #include<conio.h>
